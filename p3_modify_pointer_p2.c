738a12941f8185731bf8984150870dd6ce64dd0b
Arpan Shukla6 <arpanshukla004@gmail.com>
2025-12-23 23:28:57 +0530

    Add files via upload

---
+++ p3_modify_pointer_p2.c
@@ -0,0 +1,14 @@
+// m to modify values using pointers
+
+#include <stdio.h>
+int main(){
+    int x = 10;
+    int *ptr = &x;
+    printf("%d\n",x);
+    x = x + 30;
+    printf("%d\n",x);
+    printf("%d\n",*ptr);
+    *ptr = *ptr + 40;
+    printf("%d\n",x);
+    return 0;
+}
